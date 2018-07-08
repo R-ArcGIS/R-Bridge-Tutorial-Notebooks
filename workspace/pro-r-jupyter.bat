4a7e3f48af84e0e183e1bf5c032a50fc00f6021d
marj8502 <mpobuda@esri.com>
2018-07-07 17:22:34 -0700

    Files for running jupyter notebooks locally

---
+++ workspace/pro-r-jupyter.bat
@@ -0,0 +1,5 @@
+cd C:
+cd "\Program Files\ArcGIS\Pro\bin\Python\Scripts"
+call activate arcgispro-r
+cd "\Workspace\UC-2018-master"
+jupyter notebook .
---
+++ workspace/pro-r.bat
@@ -0,0 +1,4 @@
+cd C:
+cd "\Program Files\ArcGIS\Pro\bin\Python\Scripts"
+call activate arcgispro-r
+cd "\Workspace\UC-2018-master"
